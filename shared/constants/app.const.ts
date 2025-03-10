42b3dda1078c8a062f286bb026ee7c61425bba84
dev0715 <webdev0715@gmail.com>
2025-03-10 15:08:23 -0500

    udpate: metadata

--- shared/constants/app.const.ts
+++ shared/constants/app.const.ts
@@ -15,7 +15,7 @@ export const AppConfig = {
 export const appMetadata: Metadata = {
   title: "Fusion Studios - Dance Classes in Coín",
   description:
-    "Join Fusion Studios in Coín, Málaga, for a wide range of dance and fitness classes, including ballet, aerial silks, Brazilian Jiu-Jitsu, yoga, and more! Perfect for families, kids, and adults. Sign up for free classes and updates.",
+    "Experience the joy of dance at Fusion Studios in Coín, Málaga, Costa del Sol! Our world-class instructors offer a variety of dance classes, from ballet to Brazilian Jiu-Jitsu, designed for all ages.",
   keywords: [
     "dance classes",
     "ballet",
