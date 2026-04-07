e8afab0e759a2a6727104a07cab26d5ef1f9725b
jenarolopez <jenjenlopez37@gmail.com>
2026-04-07 18:23:12 +0800

    create class

---
+++ types/teacher.types.ts
@@ -0,0 +1,8 @@
+export interface Teacher {
+  id: string;
+  name: Record<string, string>;
+  profession: Record<string, string>;
+  description: Record<string, string>;
+  image: string;
+  position: number;
+}
